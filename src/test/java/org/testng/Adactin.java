a6097481ea363b59788d0b6d89f0cb78d5097287
jananieswar <jananisoundara@gmail.com>
2021-04-28 09:45:47 +0530

    class1 is updated

--- src/test/java/org/testng/Adactin.java
+++ src/test/java/org/testng/Adactin.java
@@ -12,6 +12,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
+import org.pom.BookHotelPayment4;
 import org.pom.SearchHotel2;
 import org.pom.SelectDetail3;
 import org.testng.annotations.AfterClass;
@@ -219,7 +220,28 @@ public void test5()  {
 }
 @Test
 private void test6() {
-	// TODO Auto-generated method stub
+	 BookHotelPayment4 payment4 =new BookHotelPayment4();
+	    WebElement txtFirstName = payment4.getTxtFirstName();
+	    enterText(txtFirstName, "janani");
+	    WebElement txtLastName = payment4.getTxtLastName();
+	    enterText(txtLastName, "eswar");
+	    WebElement txtBillingAddress = payment4.getTxtBillingAddress();
+	   enterText(txtBillingAddress, "chennai");
+	    WebElement enterCardNo = payment4.getTxtEnterCardNo();
+	    enterText(enterCardNo,"1234567890123456");
+	    WebElement btnCardType = payment4.getBtnCardType();
+	    BaseClass.dropDown(btnCardType);
+	    selectIndex(2);
+	    WebElement btnExpiryMonth = payment4.getBtnExpiryMonth();
+	    dropDown(btnExpiryMonth);
+	    selectIndex(8);
+	    WebElement btnExpiryYear = payment4.getBtnExpiryYear();
+	    dropDown(btnExpiryYear);
+	    selectByText("2021");
+	    WebElement btnEnterCvv = payment4.getBtnEnterCvv();
+	    enterText(btnEnterCvv,"123");
+	    WebElement btnclick = payment4.getBtnclick();
+	    BaseClass.btnClick(btnclick);
 
 }
 
